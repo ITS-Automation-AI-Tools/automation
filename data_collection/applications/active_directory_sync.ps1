34afc66c26b793a339d6e1476a6ec64300361666
Andrew Doria <andrew.doria@itsolutions-inc.com>
2025-07-15 17:06:01 -0700

    'init'

---
+++ data_collection/applications/active_directory_sync.ps1
@@ -0,0 +1,45 @@
+﻿$ErrorActionPreference = "STOP"
+try {
+    Import-Module ADSync
+        
+    $connectors = Get-ADSyncConnector | ?{$_.ConnectorTypeName -eq 'AD'}
+    $sync_schedule = Get-ADSyncScheduler
+    $data = @()
+    ForEach($connector in $connectors){
+        $data += [pscustomobject] @{
+            ComputerName = (Get-CimInstance Win32_ComputerSystem).Name
+                                
+            ConnectorType = $connector.ConnectorTypeName
+            Identifier = $connector.Identifier
+            Version = $connector.Version
+            Name = $connector.Name
+            CreatedAt = $connector.CreationTime
+            UpdatedAt = $connector.LastModificationTime
+            ObjectInclusionList = $connector.ObjectInclusionList
+
+            SyncIntervalMinutes = $sync_schedule.AllowedSyncCycleInterval.TotalMinutes
+            SyncIntervalSeconds = $sync_schedule.AllowedSyncCycleInterval.TotalSeconds
+            NextSyncType = $sync_schedule.NextSyncCyclePolicyType
+            NextSyncTime = $sync_schedule.NextSyncCycleStartTimeInUTC
+            SyncEnabled = $sync_schedule.SyncCycleEnabled
+            SyncMaintenanceEnable = $sync_schedule.MaintenanceEnabled
+            SchedulerSuspended = $sync_schedule.SchedulerSuspended
+        }             
+    }
+
+
+    $output = [pscustomobject]@{
+        status = "succeeded"
+        result = $data
+    }
+
+
+} catch {
+    $output = [pscustomobject]@{
+        message = $_.exception.message
+        status = "failed"
+        result = $null
+    }
+}
+
+$output
