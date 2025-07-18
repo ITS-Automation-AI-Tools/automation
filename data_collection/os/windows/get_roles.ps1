0b43c5fba05577efbed011440ff79ef186341021
Andrew Doria <andrew.doria@itsolutions-inc.com>
2025-07-18 12:22:26 -0700

    swapped character that breaks sql

--- data_collection/os/windows/get_roles.ps1
+++ data_collection/os/windows/get_roles.ps1
@@ -26,7 +26,7 @@ try{
         serial_number = $serial_number
     }
 
-    $installed_features | %{ Add-Member -InputObject $state -MemberType NoteProperty -Name ([string]$_.Name).ToLower() -Value $_.Installed -Force}
+    $installed_features | %{ Add-Member -InputObject $state -MemberType NoteProperty -Name ([string]$_.Name -replace '-','_').ToLower() -Value $_.Installed -Force}
 
     $output = [pscustomobject]@{
         status = "succeeded"
@@ -44,36 +44,3 @@ try{
 
 $output
 
-
-# SIG # Begin signature block
-# MIIFlQYJKoZIhvcNAQcCoIIFhjCCBYICAQExDzANBglghkgBZQMEAgEFADB5Bgor
-# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
-# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCDADTHtQjiA9x5O
-# xBHNAoimYQtisfa/PBbrguVmcCt7+KCCAwowggMGMIIB7qADAgECAhAoyEyARsZY
-# mksZuPjiDe0aMA0GCSqGSIb3DQEBCwUAMBsxGTAXBgNVBAMMEElUUyBDb2RlIFNp
-# Z25pbmcwHhcNMjUwMzA1MjMyNzU3WhcNMjYwMzA1MjM0NzU3WjAbMRkwFwYDVQQD
-# DBBJVFMgQ29kZSBTaWduaW5nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
-# AQEA0fKohFZ3wtfJOLMaO/4VoloNfloWf8B5UjaE2sm9EQpLpP8/hTHqspXgz7zS
-# 7euCCxH97qt1kk9l3RpxbEcs1xcWnCrZryFO2F8Bvmyhtk5tE30DSREwssUwOaZL
-# McCKPEb2YerdOIUoiPdwn+nx98PLmzoNM2A25KIAmsmOUX/4TxH70GYPR8YxdrMI
-# 7mYFDMneJVZ7B9QL0JpCC9oAyR5L1a6yg4amHK9y44YWzm6yfqrzrmJlEw/I/hLc
-# pDEqZ6ohfcCdXQF4HoJx2ecusdZTE7IktgF/GJmkIfCtu6BlHZUhCoT9hksucI9z
-# sw90bbeyna4cxlbcL9FlgRwBkQIDAQABo0YwRDAOBgNVHQ8BAf8EBAMCB4AwEwYD
-# VR0lBAwwCgYIKwYBBQUHAwMwHQYDVR0OBBYEFP5GbQt+h0IBzqiQbJsxiRB+gKmT
-# MA0GCSqGSIb3DQEBCwUAA4IBAQCSYypjyMC6zh5P/OpsJw7JQIuSYtyL3/7ZymE4
-# bC4OEkN7ScdQFJyD2Yb88exHgjziVNZK0g7gnc9cwExDFfibDNWWWpzp91fcGBqa
-# FFh0PNFb3xuXB/HOGNXGxN4TixK95PiaxQtkG/abdw82kGcCbD6GcMYcPCr4XYqs
-# mASG226FdraG5BmasUmUScRlKvoUACOaa65aOlMKGSuf36wbmSA+V/rxmzQnS0Vp
-# 03VpVrHqa6uh3t5oM9zbyjf8+oysqT3BotZrkZtX6lfVFNHTympfYxf67bDQ3Pw2
-# iJ/MpHh+AQRZFQfWc9DWv7I2TdPLGvaBcAdcuvcXN+iLErPqMYIB4TCCAd0CAQEw
-# LzAbMRkwFwYDVQQDDBBJVFMgQ29kZSBTaWduaW5nAhAoyEyARsZYmksZuPjiDe0a
-# MA0GCWCGSAFlAwQCAQUAoIGEMBgGCisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJ
-# KoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQB
-# gjcCARUwLwYJKoZIhvcNAQkEMSIEIJ7GVDaP5fSG23tb696c4OvLLbMCRy6J1jEG
-# mfpy1DMoMA0GCSqGSIb3DQEBAQUABIIBAFAbytTsNqJrBgAV18MBDC1bq/aOs+Z/
-# +LMPaKPzmK91DzDuADhupXme6nPvXpeQRb0ILAeRdePCy4qTtbXwgPiS/rzOAt9a
-# ZNBMyiiZHwbEDFj/XPJvdVtBlsCCbfuJVW5xgvv6DA1IaKnlHKR2IHQBfA3zYGML
-# +Sn3xrOifACtjOjuQ2x/uWmYZk0+eTDggEc+T3sMv3XWpbA+hOL8XcGJzfAcKWw0
-# xA6Sw1L1QsHoVliyUpjJ/wcyvondftPlCIPq5Wq1JBD5uHy5Ryh9pyTANYK87eLX
-# fdFLt9G0Z0SrZJUnDgOlv8RzXKO7N+TyFNG3fjtsjdUy8hePp0Jsv+4=
-# SIG # End signature block
