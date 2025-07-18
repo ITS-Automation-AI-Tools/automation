81f6db440c570e1f71d0bb92f652de5f436c05ae
Andrew Doria <andrew.doria@itsolutions-inc.com>
2025-07-18 10:22:12 -0700

    init

---
+++ data_collection/os/windows/get_roles.ps1
@@ -0,0 +1,46 @@
+﻿function Get-Hostname {
+    Process {
+        try{
+            return (&hostname).toUpper()
+        } catch{}
+        try{
+           return (Get-ComputerInfo -Property "CsDNSHostName").CsDNSHostName.toUpper()
+        } catch{}
+        try{
+            return (Get-WmiObject win32_computersystem -Property *).Name.toUpper()
+        } catch{}
+
+        return $env:COMPUTERNAME
+    }
+}
+
+$ErrorActionPreference = "STOP"
+
+try{
+    $serial_number = (Get-WmiObject Win32_BIOS).SerialNumber
+    $hostname = Get-Hostname
+    $installed_features = Get-WindowsFeature
+
+    $state = [pscustomobject]@{
+        name = $hostname
+        serial_number = $serial_number
+    }
+
+    $installed_features | %{ Add-Member -InputObject $state -MemberType NoteProperty -Name ([string]$_.Name).ToLower() -Value $_.Installed -Force}
+
+    $output = [pscustomobject]@{
+        status = "succeeded"
+        message = $null
+        result = $state
+    }
+
+} catch {
+    $output = [pscustomobject]@{
+        status = "failed"
+        message = $_.exception.message
+        result = $null
+    }
+}
+
+$output
+
